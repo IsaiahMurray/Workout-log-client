15f6e5a4d31acd7be1a2a7fed2bae20c6be140ef
Isaiah Murray <murray.isaiah13@gmail.com>
2021-02-19 17:27:00 -0500

    fixed fetch request

--- src/components/Workouts/WorkoutIndex.js
+++ src/components/Workouts/WorkoutIndex.js
@@ -1,32 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import {Container, Row, Col} from 'reactstrap';
-import WorkoutCreate from './WorkoutCreate';
-import WorkoutTable from './WorkoutTable';
-import WorkoutEdit from './WorkoutEdit';
-import APIURL from '../../helpers/environment';
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+import WorkoutCreate from "./WorkoutCreate";
+import WorkoutTable from "./WorkoutTable";
+import WorkoutEdit from "./WorkoutEdit";
+import APIURL from "../../helpers/environment";
 
 const WorkoutIndex = (props) => {
-
   const [workouts, setWorkouts] = useState([]);
   const [updateActive, setUpdateActive] = useState(false);
   const [workoutToUpdate, setWorkoutToUpdate] = useState({});
 
   const fetchWorkouts = () => {
-    fetch(`${APIURL}log/all`, {
+    fetch(`http://localhost:3000/log/all`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
         Authorization: props.token,
       },
     })
-    .then( (res) => {
-      res.json();
-      console.log(res)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
     })
-    .then((data) => {
-      console.log("fetch data: ", data)
-      //setWorkouts(logData)
-    })
+    .then(data => console.log(data))
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
   };
 
   const editUpdateWorkout = (workout) => {
@@ -40,29 +41,38 @@ const WorkoutIndex = (props) => {
 
   const updateOff = () => {
     setUpdateActive(false);
-  }
+  };
 
   useEffect(() => {
     fetchWorkouts();
-  }, [])
+  }, []);
 
-  console.log(workouts)
+  console.log(workouts);
 
- return(
-   <div>
-    <Container>
-      <Row>
-        <Col md="3">
-          <WorkoutCreate fetchWorkouts={fetchWorkouts} token={props.token} />
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col md="3">
+            <WorkoutCreate fetchWorkouts={fetchWorkouts} token={props.token} />
+          </Col>
+          <Col md="9">
+          {(!workouts.length === 0) ? <WorkoutTable workouts={workouts} editUpdateWorkout={editUpdateWorkout} updateOn={updateOn} fetchWorkouts={fetchWorkouts} token={props.token}/> : <h1>Log a workout!</h1>}
         </Col>
-        {/* <Col md="9">
-          {(workouts.length) ? <WorkoutTable workouts={workouts} editUpdateWorkout={editUpdateWorkout} updateOn={updateOn} fetchWorkouts={fetchWorkouts} token={props.token}/> : <h1>Log a workout!</h1>}
-        </Col> */}
-        {updateActive ? <WorkoutEdit workoutToUpdate={workoutToUpdate} updateOff={updateOff} token={props.token} fetchWorkouts={fetchWorkouts} /> : <div></div>}
-      </Row>
-    </Container>
-   </div>
- )
-}
+          {updateActive ? (
+            <WorkoutEdit
+              workoutToUpdate={workoutToUpdate}
+              updateOff={updateOff}
+              token={props.token}
+              fetchWorkouts={fetchWorkouts}
+            />
+          ) : (
+            <div></div>
+          )}
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default WorkoutIndex;
