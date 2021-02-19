7da084c01a77f8275d70d3074b54264af398679d
Isaiah Murray <murray.isaiah13@gmail.com>
2021-02-19 15:25:39 -0500

    Updated nav styling

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -6,29 +6,48 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  Button
+  
 } from 'reactstrap';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  logout: {
+    
+  },
+  title: {
+    flexGrow: 1,
+  },
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 const Sitebar = (props) => {
 
   const [isOpen, setIsOpen] = useState(false);
-
+  const classes = useStyles();
   const toggle = () => {
     setIsOpen(!isOpen);
   }
 
   return(
-    <Navbar color="faded" light expand="md">
-      <NavbarBrand href="/">Workout Log</NavbarBrand>
-      <NavbarToggler onClick={toggle}/>
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-            <NavItem>
-              <Button onClick={props.clearToken()}>Logout</Button>
-            </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
+    <div className={classes.root}>
+      <AppBar id="app-bar" position="static">
+      <Toolbar>
+        <Typography className={classes.title} variant="h6">
+          Workout
+        </Typography>
+        <Button className={classes.logout} id="logout-button" onClick={props.clearToken}>Logout</Button>
+      </Toolbar>
+    </AppBar>
+    </div>
+    
   );
 };
 
