82fd14375e759d46dc61dbf904fa68be52123c6d
Isaiah Murray <murray.isaiah13@gmail.com>
2021-02-19 13:19:09 -0500

    updated environment file

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,11 +2,11 @@ let APIURL = '';
 
 switch (window.location.hostname) {
   case 'localhost' || '127.0.0.1':
-    APIURL = 'http://localhost:3001'
+    APIURL = 'http://localhost:3000'
     break;
 
   case 'wd54-client-deploy.herokuapp.com':
-    APIURL = 'https://dlg-blue-deploy-server.herokuapp.com'
+    APIURL = ''
 }
 
 export default APIURL;
